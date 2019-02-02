daa9157841a64109a25110603cc6e5a0cae2bbdf
Trim Bresilla <trim.bresilla@gmail.com>
2019-02-02 23:20:57 +0100

    comit after fork

--- build.js
+++ build.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 
 saveTheme();
 
-fs.watch('source/theme.yaml', saveTheme);
+fs.watch('source/dark.yaml', saveTheme);
 
 function saveTheme(curr, prev) {
-    const theme_file = fs.readFileSync('./source/theme.yaml', 'utf8');
+    const theme_file = fs.readFileSync('./source/dark.yaml', 'utf8');
     const yaml_obj = yaml.load(theme_file);
     const json_theme = JSON.stringify(yaml_obj, null, 4);
 
-    fs.writeFile('./themes/theme.json', json_theme, (err) => {
+    fs.writeFile('./themes/Dark.json', json_theme, (err) => {
         if (err) {
             throw err;
         }
